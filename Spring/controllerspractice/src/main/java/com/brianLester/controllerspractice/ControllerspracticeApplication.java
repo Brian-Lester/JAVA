1999d4b81dd918694fb34e3d403b872f954c76c8
Brian-Lester <blester426@gmail.com>
2023-01-23 21:39:38 -0500

    Spring

---
+++ Spring/controllerspractice/src/main/java/com/brianLester/controllerspractice/ControllerspracticeApplication.java
@@ -0,0 +1,15 @@
+package com.brianLester.controllerspractice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ControllerspracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerspracticeApplication.class, args);
+	}
+
+}
