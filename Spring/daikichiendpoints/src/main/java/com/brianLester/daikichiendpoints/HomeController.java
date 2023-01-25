64dbc8f1daf5ff428360971344725b9013c95ee8
Brian-Lester <blester426@gmail.com>
2023-01-24 21:28:52 -0500

    commit

--- Spring/daikichiendpoints/src/main/java/com/brianLester/daikichiendpoints/HomeController.java
+++ Spring/daikichiendpoints/src/main/java/com/brianLester/daikichiendpoints/HomeController.java
@@ -1,6 +1,9 @@
 package com.brianLester.daikichiendpoints;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/daikichi")
@@ -17,6 +20,22 @@ public class HomeController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Big things are waiting for you!";
+	}
+	
+	@GetMapping("/travel/{city}")
+	public String Travel(@PathVariable("city") String city) {
+		return "Congats you won a trip to " + city +"!";
+		
+	}
+	
+	@GetMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
 	}
 
 }
