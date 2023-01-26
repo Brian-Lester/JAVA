c9b9a04414b153637d0819ee5f63c32e1631fc6e
Brian-Lester <blester426@gmail.com>
2023-01-26 15:12:42 -0500

    commit

---
+++ Spring/hoppersrecipt/src/main/java/com/brianLester/hoppersrecipt/HoppersreciptApplication.java
@@ -0,0 +1,13 @@
+package com.brianLester.hoppersrecipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersreciptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersreciptApplication.class, args);
+	}
+
+}
---
+++ Spring/hoppersrecipt/src/test/java/com/brianLester/hoppersrecipt/HoppersreciptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianLester.hoppersrecipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreciptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
